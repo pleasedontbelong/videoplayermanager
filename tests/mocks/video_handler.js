88c0d0f22edac19977baed17982481a46b696850
Pleasedontbelong <pleasedontbelong@gmail.com>
2014-12-16 00:18:43 +0100

    Add tests for firing ticks

--- tests/mocks/video_handler.js
+++ tests/mocks/video_handler.js
@@ -1,7 +1,6 @@
 module.exports = (function() {
     'use strict';
     function VideoHandler(options) {
-        console.log("video handler init");
     }
 
     return VideoHandler;
--- tests/test_main.js
+++ tests/test_main.js
@@ -1,23 +1,86 @@
 describe('VideoManager', function(){
     describe('getUrl', function(){
-        it('should return the crawl endpoint', function(){
+        it('should fire the tick', function(){
             var VideoHandler = require("./mocks/video_handler.js");
             VideoHandler.prototype.getCurrentTime = sinon.stub().returns(10);
 
             var callback = sinon.stub();
-            
+
+            var ticks = ['0:10', '0:30', '1:10'];
+
             var manager = new VideoPlayerManager.Manager({
                 'handler': VideoHandler,
-                'ticks': [
-                    {'time': '0:10', 'return': "5 sec something"},
-                    {'time': '0:30', 'return': "30 sec something"},
-                    {'time': '1:10', 'return': "1 min 10 sec something"},
-                ],
+                'ticks': ticks,
                 'onTick': callback
             });
             manager.checkTicks();
             expect(callback.calledOnce).to.be.true;
-            expect(callback.calledWith({'time': '0:10', 'return': "5 sec something"})).to.be.true;
+            expect(callback.calledWith('0:10', ticks)).to.be.true;
+        });
+
+        it('should fire the tick when manually moved', function(){
+            var VideoHandler = require("./mocks/video_handler.js");
+            var mocked_function = sinon.stub();
+            mocked_function.onFirstCall().returns(30);
+            mocked_function.onSecondCall().returns(15); // maybe manually moved by the user
+            mocked_function.returns(false);
+            VideoHandler.prototype.getCurrentTime = mocked_function;
+
+            var callback = sinon.stub();
+
+            var ticks = ['0:10', '0:30', '1:10'];
+
+            var manager = new VideoPlayerManager.Manager({
+                'handler': VideoHandler,
+                'ticks': ticks,
+                'onTick': callback
+            });
+            manager.checkTicks();
+            expect(callback.calledOnce).to.be.true;
+            expect(callback.calledWith('0:30', ticks)).to.be.true;
+
+            manager.checkTicks();
+            expect(callback.calledWith('0:10', ticks)).to.be.true;
+        });
+
+        it('should fire last tick', function(){
+            var VideoHandler = require("./mocks/video_handler.js");
+            var mocked_function = sinon.stub().returns(80);
+
+            VideoHandler.prototype.getCurrentTime = mocked_function;
+
+            var callback = sinon.stub();
+
+            var ticks = ['0:10', '0:30', '1:10'];
+
+            var manager = new VideoPlayerManager.Manager({
+                'handler': VideoHandler,
+                'ticks': ticks,
+                'onTick': callback
+            });
+            manager.checkTicks();
+            expect(callback.calledOnce).to.be.true;
+            expect(callback.calledWith('1:10', ticks)).to.be.true;
+        });
+
+        it('should fire first tick', function(){
+            var VideoHandler = require("./mocks/video_handler.js");
+            var mocked_function = sinon.stub().returns(0);
+
+            VideoHandler.prototype.getCurrentTime = mocked_function;
+
+            var callback = sinon.stub();
+
+            var ticks = ['0:00', '0:10', '0:30', '1:10'];
+
+            var manager = new VideoPlayerManager.Manager({
+                'handler': VideoHandler,
+                'ticks': ticks,
+                'onTick': callback
+            });
+            manager.checkTicks();
+            expect(callback.calledOnce).to.be.true;
+            expect(callback.calledWith('0:00', ticks)).to.be.true;
         });
     });
 });
